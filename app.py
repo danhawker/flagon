5283800811cde8d0357a9b93fe7c7758ad9a96de
lv31427 <Daniel.Hawker@lv.com>
2016-03-22 15:50:12 +0000

    Add few JSON examples/tests

--- app.py
+++ app.py
@@ -18,4 +18,48 @@ def json(id):
     id=id
     return { "id": id, "name": "Test Item " + str(id) }
 
+@route('/documents', method='PUT')
+def put_document():
+    data = request.body.readline()
+    if not data:
+        abort(400, 'No data received')
+#    entity = json.loads(data)
+#    if not entity.has_key('id'):
+#        abort(400, 'No id specified')
+    try:
+        return { data }
+
+#        db['documents'].save(entity)
+    except ValidationError as ve:
+        abort(400, str(ve))
+
+@route('/jsontest', method='PUT')
+def put_json():
+    data = request.json()
+    if not data:
+        abort(400, 'No data received')
+    try:
+        #return { data }
+        return { data.id }
+    except ValidationError as ve:
+        abort(400, str(ve))
+
+@route('/sendsms', method='PUT')
+def send_sms():
+    recipient = request.json['recipient']
+    body = request.json['body']
+    
+    if not recipient:
+        abort(400, 'No Recipient Defined')
+        
+    if not body:
+        abort(400, 'No Message Body Defined')
+    
+    try:
+        return {"recipient": recipient, "body": body}
+        
+    except ValidationError as ve:
+        abort(400, str(ve))
+            
+
 run(host='localhost', port=8080)
